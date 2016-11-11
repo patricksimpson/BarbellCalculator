9c0dd7f5ec628c268ecf078965e4a399d8a55a6b
Patrick Simpson <izerop@gmail.com>
2016-11-11 11:57:26 -0500

    feat: adding totals for calculations

--- app/BarbellPlateButton.js
+++ app/BarbellPlateButton.js
@@ -11,26 +11,26 @@ import styles from './Styles';
 export default class BarbellPlateButton extends Component {
 
   _getKey() {
-    return '@BarbellCalculatorApp:' + this.props.amount;
+    return '@BarbellCalculatorApp:' + this.props.label;
   }
 
   _sub() {
-    if (this.state.value > 0) {
-      this._updateStateValue(-1);
+    if (this.state.amount> 0) {
+      this._updateStateAmount(-1);
     }
   }
 
   _add() {
-    if (this.state.value < 50) {
-     this._updateStateValue(1);
+    if (this.state.amount< 50) {
+     this._updateStateAmount(1);
     }
   }
-  _updateStateValue(n)  {
-    this._saveState((this.state.value * 1) + n);
+  _updateStateAmount(n)  {
+    this._saveState((this.state.amount* 1) + n);
   }
 
   _saveState(n) {
-    this.setState({value: n});
+    this.setState({amount: n});
     AsyncStorage.setItem(this._getKey(), JSON.stringify(n));
   }
 
@@ -40,18 +40,18 @@ export default class BarbellPlateButton extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { value: 0 }
+    this.state = { amount: 0 }
   }
 
   _loadInitialState = async () => {
     try {
       var key = this._getKey();
-      var value = await AsyncStorage.getItem(key);
-      if (value !== null){
-        this.setState({value: value});
+      var amount = await AsyncStorage.getItem(key);
+      if (amount !== null){
+        this.setState({amount: amount});
       } else {
         AsyncStorage.setItem(key, JSON.stringify(2));
-        this.setState({value: 2});
+        this.setState({amount: 2});
       }
     } catch (error) {
       alert(error.message);
@@ -62,7 +62,7 @@ export default class BarbellPlateButton extends Component {
     return (
       <View style={styles.plateButtonContainer}>
         <TouchableHighlight onPress={ this._sub.bind(this) }><Text style={ styles.plateButton }>-</Text></TouchableHighlight>
-        <Text sryle={styles.plateText}>{this.props.amount} x {this.state.value}</Text>
+        <Text sryle={styles.plateText}>{this.props.label} x {this.state.amount}</Text>
         <TouchableHighlight onPress={ this._add.bind(this) }><Text style={ styles.plateButton }>+</Text></TouchableHighlight>
       </View>
     );
--- app/BarbellPlates.js
+++ app/BarbellPlates.js
@@ -4,6 +4,7 @@ import {
   View,
   Navigator,
   TouchableHighlight,
+  AsyncStorage,
 } from 'react-native';
 
 import BarbellInput from './BarbellInput';
@@ -12,8 +13,43 @@ import BarbellPlateButton from './BarbellPlateButton';
 
 export default class BarbellPlates extends Component {
 
+
+  plates() {
+    return [
+    { value: 45, label: '45' },
+    { value: 35, label: '35' },
+    { value: 25, label: '25' },
+    { value: 10, label: '10' },
+    { value: 5, label: '5' },
+    { value: 2.5, label: '2-5' },
+    ];
+  }
+
   _navigate() {
     this.props.navigator.pop()
+  }
+
+  _getTotal = async () => {
+    const PREFIX = '@BarbellCalculatorApp:'
+    var total = this.state.total;
+    for (let plate of this.plates()) {
+      try {
+        var amount = await AsyncStorage.getItem(PREFIX + plate.label);
+        total = (amount * plate.value) + total;
+        this.setState({ total: total });
+      } catch(error) {
+        alert(error.message);
+      }
+    }
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { total: 0 };
+  }
+
+  componentDidMount() {
+    this._getTotal().done();
   }
 
   render() {
@@ -27,14 +63,15 @@ export default class BarbellPlates extends Component {
             Edit Plates
           </Text>
         </View>
+        <Text style={styles.instructions}>{ this.state.total } lbs</Text>
         <View style={styles.content}>
           <View style={styles.plateInventory}>
-            <BarbellPlateButton amount='45'/>
-            <BarbellPlateButton amount='35'/>
-            <BarbellPlateButton amount='25'/>
-            <BarbellPlateButton amount='10'/>
-            <BarbellPlateButton amount='5'/>
-            <BarbellPlateButton amount='2.5'/>
+            <BarbellPlateButton value='45' label='45'/>
+            <BarbellPlateButton value='35' label='35'/>
+            <BarbellPlateButton value='25' label='25'/>
+            <BarbellPlateButton value='10' label='10'/>
+            <BarbellPlateButton value='5' label='5'/>
+            <BarbellPlateButton value='2.5' label='2-5'/>
           </View>
         </View>
       </View>
